2c86b159ad643eb1b2453ed72fd395e7a7a29231
liujc <liujc@unionpaysmart.com>
2014-08-20 15:02:58 +0800

    init project

---
+++ word-count/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+hadoop jar /usr/lib/hadoop-0.20-mapreduce/contrib/streaming/hadoop-streaming.jar \
+	-D mapred.job.name="my test word count" \
+	-numReduceTasks 2 \
+	-file $PWD/mapper.py -mapper $PWD/mapper.py \
+	-file $PWD/reducer.py -reducer $PWD/reducer.py \
+	-input /user/hduser/jazz/wordcount.txt -output /user/hduser/jazz/wordcount.output.4
+
---
+++ word-count/test.sh
@@ -0,0 +1,1 @@
+echo "hello world hello jazz" | ./mapper.py | sort -k1,1 | ./reducer.py 
