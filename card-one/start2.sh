63ec9a8fbf70f7a2b11b8507ae62ca246da7b758
zhijia,.zhang <jiahut@gmail.com>
2014-09-03 14:48:14 +0800

    add the start bash

---
+++ card-one/start2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+hadoop jar /home/upsmart/tools/hadoop-2.5.0/share/hadoop/tools/lib/hadoop-streaming-*.jar \
+	-D mapred.job.name="diff job " \
+	-numReduceTasks 1 \
+	-input $1 \
+	-output $2  \
+	-file $PWD/mapper.py -mapper $PWD/mapper.py \
+	-file $PWD/reducer.py -reducer $PWD/reducer.py 
